762f1f3da9ba8e3a9989bc736f3f499a7a3bfb33
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-09-05 19:31:25 +0530

    Python backend code for simple server

---
+++ Python_Backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Python Backend Server is running."})
+
+if __name__ == '__main__':
+    app.run(debug=True)
